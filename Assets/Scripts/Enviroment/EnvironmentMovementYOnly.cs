ad3060b57cc32d4981a252a5697545e3918b7817
Unknown <wndtanaka@gmail.com>
2017-08-24 11:26:20 +1000

    MVP commit

---
+++ Assets/Scripts/Enviroment/EnvironmentMovementYOnly.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentMovementYOnly : MonoBehaviour
+{
+    // Variables
+    public float rotationSpeed = 20f;
+    public float deceleration = 10f;
+    public float inputV;
+    public float inputH;
+    public float minRot = -10f;
+    public float maxRot = 10f;
+    public const float yRot = 0;
+
+    private Rigidbody rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 5;
+        rigid = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call deceleration and rotation
+       
+        Rotation();
+        Decelerate();
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+    }
+
+
+    void Decelerate()
+    {
+        // Activates deceleration
+        rigid.velocity += -rigid.velocity * deceleration * Time.deltaTime;
+    }
+
+    void Rotation()
+    {
+        inputV = Input.GetAxis("Vertical") * rotationSpeed * Time.deltaTime;
+        inputH = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        //transform.Rotate(0, -inputV, 0);
+        transform.Rotate(0, inputH, 0);
+
+        //Vector3 currentRotation = transform.localRotation.eulerAngles;
+        //currentRotation.x = Mathf.Clamp(currentRotation.x, minRot, maxRot);
+        //currentRotation.z = Mathf.Clamp(currentRotation.z, minRot, maxRot);
+        //transform.localRotation = Quaternion.Euler(currentRotation);
+
+
+    }
+}
